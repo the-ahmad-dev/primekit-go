b8af1151c886318540ea7ad1b596aae8be13b351
Ahmad Ali <ahmd.ali.sabir@gmail.com>
2025-02-24 01:35:01 +0500

    random

--- random/random.go
+++ random/random.go
@@ -1,28 +1,36 @@
 package random
 
 import (
+	"bytes"
 	"math/rand"
 	"time"
 )
 
-// RandomInt returns a random integer between min and max (inclusive)
-func RandomInt(min, max int) int {
-	return rand.Intn(max-min+1) + min
-}
+var defLetters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var defLettersRune = []rune(defLetters)
 
-const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-
-// RandomString returns a random string of the specified length
-func RandomString(length int) string {
-	b := make([]byte, length)
-	for i := range b {
-		b[i] = charset[rand.Intn(len(charset))]
+func String(n int, letters ...string) string {
+	// Default letters if none are passed.
+	var letterRunes []rune
+	if len(letters) > 0 {
+		letterRunes = []rune(letters[0])
+	} else {
+		letterRunes = defLettersRune
 	}
-	return string(b)
-}
 
-// SecureRandomInt returns a cryptographically secure random integer
-func SecureRandomInt(min, max int) (int, error) {
-	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
-	return rng.Intn(max-min+1) + min, nil
+	// Create a random source.
+	randSource := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(randSource)
+
+	var bb bytes.Buffer
+	bb.Grow(n)
+	l := len(letterRunes)
+
+	// Generate random index and append.
+	for i := 0; i < n; i++ {
+		randomIndex := r.Intn(l)
+		bb.WriteRune(letterRunes[randomIndex])
+	}
+
+	return bb.String()
 }
